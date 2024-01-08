b2bc13534f39a0f16f17456ac524154c42a2679f
Niels Eppenhof <n.eppenhof@gmail.com>
2024-01-08 20:41:55 +0100

    Added linux (x11) support

--- premake5.lua
+++ premake5.lua
@@ -6,7 +6,6 @@ project "glfw"
 	objdir ("%{wks.location}/Bin-Int/" .. OutputDir .. "/%{prj.name}")
 
 	defines {
-		"_GLFW_WIN32",
 		"_CRT_SECURE_NO_WARNINGS"
 	}
 
@@ -20,23 +19,49 @@ project "glfw"
 		"src/null_joystick.c",
 		"src/null_monitor.c",
 		"src/null_window.c",
-		"src/osmesa_context.c",
 		"src/platform.c",
 		"src/vulkan.c",
-		"src/wgl_context.c",
-		"src/win32_init.c",
-		"src/win32_joystick.c",
-		"src/win32_module.c",
-		"src/win32_monitor.c",
-		"src/win32_thread.c",
-		"src/win32_time.c",
-		"src/win32_window.c",
 		"src/window.c"
 	}
 
 	includedirs {
 		"include/GLFW"
 	}
+
+	filter "system:windows"
+		defines {
+			"_GLFW_WIN32"
+		}
+
+		files {
+			"src/wgl_context.c",
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_module.c",
+			"src/win32_monitor.c",
+			"src/win32_thread.c",
+			"src/win32_time.c",
+			"src/win32_window.c"
+		}
+	
+	filter "system:linux"
+		defines {
+			"_GLFW_X11"
+		}
+
+		files {
+			"src/egl_context.c",
+			"src/glx_context.c",
+			"src/linux_joystick.c",
+			"src/posix_module.c",
+			"src/posix_poll.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c"
+		}
 
 	filter "configurations:Debug"
 		runtime "Debug"
